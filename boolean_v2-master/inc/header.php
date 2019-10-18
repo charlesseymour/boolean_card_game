f9a1b2807270696e499204e2ec36cadc9b413732
Charles Seymour <charlesseymour@yahoo.com>
2019-10-18 14:10:24 -0500

    Added register link to header

--- boolean_v2-master/inc/header.php
+++ boolean_v2-master/inc/header.php
@@ -46,6 +46,9 @@ require_once 'bootstrap.php';
 		<li class="nav-item">
 			<a class="nav-link" href="/login.php">Login</a>
 		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="/register.php">Register</a>
+		</li>
 	<?php endif ?>
     </ul>	
   </div>
