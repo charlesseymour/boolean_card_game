3449dab975a95c34ce00556c2c16787e3859b4b3
Charles Seymour <charlesseymour@yahoo.com>
2019-10-21 17:42:02 -0500

    Narrowed play history column, added win percentage

--- boolean_v2-master/public/account.php
+++ boolean_v2-master/public/account.php
@@ -4,39 +4,52 @@ include '../inc/header.php';
 
 requireAuth();
 
-?>
+$userId = decodeJwt('sub');
+$stmt = $db->prepare("SELECT * FROM plays WHERE user_id = " . $userId . " ORDER BY id DESC");
+$stmt->execute();
+$results = $stmt->fetchAll();
+$wins = array_filter($results, function($v){
+	return $v['win'] == 1;
+});
 
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Date</th>
-      <th scope="col">Boolean</th>
-      <th scope="col">Outcome</th>
-    </tr>
-  </thead>
-  <tbody>
-	<?php 
-		$userId = decodeJwt('sub');
-		$stmt = $db->prepare("SELECT * FROM plays WHERE user_id = " . $userId);
-		$stmt->execute();
-		$results = $stmt->fetchAll();
-		foreach ($results as $result) {
-			$date = $result['date'];
-			$mode = strtoupper($result['mode']);
-			if ($result['win'] == 0) {
-				$outcome = 'lose';
-			} else {
-				$outcome = 'win';
-			}
-			echo <<<EOD
-			<tr>
-				<td>$date</td>
-				<td>$mode</td>
-				<td>$outcome</td>
-			</tr>
+?>
+<div class="container">
+	<div class="row pt-5">
+		<h1 class="mx-auto">Play history</h1>
+	</div>
+	<div class="row pb-5">
+		<h6 class="mx-auto">Win percentage: <?php echo round((count($wins) / count($results)) * 100); ?>%</h6>
+	</div>
+</div>
+<div class="container">
+	<table class="table table-striped">
+	  <thead>
+		<tr>
+		  <th scope="col">Date</th>
+		  <th scope="col">Boolean</th>
+		  <th scope="col">Outcome</th>
+		</tr>
+	  </thead>
+	  <tbody>
+		<?php 
+			foreach ($results as $result) {
+				$date = $result['date'];
+				$mode = strtoupper($result['mode']);
+				if ($result['win'] == 0) {
+					$outcome = 'lose';
+				} else {
+					$outcome = 'win';
+				}
+				echo <<<EOD
+				<tr>
+					<td>$date</td>
+					<td>$mode</td>
+					<td>$outcome</td>
+				</tr>
 EOD;
-		
-		}
-	?>
-  </tbody>
-</table>
+			
+			}
+		?>
+	  </tbody>
+	</table>
+</div>
